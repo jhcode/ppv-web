0624a62c3958d051eb5261752c47fa97cfe13395
Mofesola <me@mofesola.com>
2017-10-31 14:40:36 +0100

    Add EFS mount to setup

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -2,3 +2,17 @@ provider "aws" {
   region = "eu-west-1"
   profile = "mt"
 }
+
+provider "ignition" {
+  version = "0.1.0"
+}
+
+module "efs" {
+  source = "git::https://bitbucket.org/mettalloids/terraform-modules//efs"
+  performance_mode = "maxIO"
+  aws_region = "eu-west-1"
+  subnets = "${split(",", var.subnets)}"
+  internal_sg_id = "${aws_security_group.internal.id}"
+  cluster_name = "${var.ecs_cluster_name}"
+  project_billing_id = "${var.project_billing_id}"
+}
