c0dda5ec42605bd8a75a21748ba03a5334a5dc5f
Mofesola Babalola <mofesolababalola@mettalloids.com>
2018-02-28 17:29:28 +0000

    Merged in conf/env (pull request #10)
    Conf/env
    
    * Migrate app to new cluster
    
    * Migrate app to new cluster
    
    * Migrate app to new cluster
    
    * Migrate app to new cluster. Deploy to develop and prod
    
    * Migrate app to new cluster. Deploy to develop and prod
    
    * Migrate app to new cluster. Deploy to develop and prod

--- deploy/prod/build-prod.sh
+++ deploy/prod/build-prod.sh
@@ -6,18 +6,18 @@ eval $(aws ecr get-login --no-include-email --region ${AWS_REGION})
 
 
 ##############################################
-## PECULIAR-WP-APP
+## PECULIAR-PPV-APP
 ##############################################
 
-docker build -t peculiar-wp-prod .
-docker tag peculiar-wp-prod:latest 780172389235.dkr.ecr.eu-west-1.amazonaws.com/peculiar-wp-peculiar-wp-prod:latest
-docker push 780172389235.dkr.ecr.eu-west-1.amazonaws.com/peculiar-wp-peculiar-wp-prod:latest
+docker build -t peculiar-ppv-prod .
+docker tag peculiar-ppv-prod:latest 780172389235.dkr.ecr.eu-west-1.amazonaws.com/peculiar-ppv-peculiar-ppv-prod:latest
+docker push 780172389235.dkr.ecr.eu-west-1.amazonaws.com/peculiar-ppv-peculiar-ppv-prod:latest
 
 
 ##############################################
 ## NGINX
 ##############################################
 
-docker build -t peculiar-wp-prod-nginx nginx/.
-docker tag peculiar-wp-prod-nginx:latest 780172389235.dkr.ecr.eu-west-1.amazonaws.com/peculiar-wp-peculiar-wp-nginx-prod:latest
-docker push 780172389235.dkr.ecr.eu-west-1.amazonaws.com/peculiar-wp-peculiar-wp-nginx-prod:latest
+docker build -t peculiar-ppv-prod-nginx nginx/.
+docker tag peculiar-ppv-prod-nginx:latest 780172389235.dkr.ecr.eu-west-1.amazonaws.com/peculiar-ppv-peculiar-ppv-nginx-prod:latest
+docker push 780172389235.dkr.ecr.eu-west-1.amazonaws.com/peculiar-ppv-peculiar-ppv-nginx-prod:latest
