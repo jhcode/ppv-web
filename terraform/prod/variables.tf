ba915cb71e3b39b3232c614b0824f036defbd066
Baba Hassan <jide20002003@gmail.com>
2018-02-28 18:50:52 +0100

    Change project name

--- terraform/prod/variables.tf
+++ terraform/prod/variables.tf
@@ -1,6 +1,6 @@
 variable "environment" { default = "prod" }
-variable "project" { default = "picogauge-api" }
-variable "project_billing_id" { default = "PICOGAUGE" }
+variable "project" { default = "peculiar-ppv" }
+variable "project_billing_id" { default = "PECULIAR" }
 variable "aws_region" { default = "eu-west-1" }
 
 # RDS
