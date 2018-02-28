70d96a31872a9e8b25a6083f46dd38471f1258e0
Mofesola Babalola <mofesolababalola@mettalloids.com>
2018-02-28 17:17:06 +0000

    Merged in conf/env (pull request #8)
    Migrate app to new cluster
    
    * Migrate app to new cluster
    
    * Migrate app to new cluster
    
    * Migrate app to new cluster

---
+++ terraform/dev/main.tf
@@ -0,0 +1,16 @@
+data "credstash_secret" "password" {                                                                                                                                                                                                                          
+  name = "${var.project}-${var.environment}-couchdb-password"                                                                                                                                                                                                       
+}
+
+module "peculiar-ppv" {
+  source                    = "git::ssh://git@bitbucket.org/mettalloids/terraform-modules.git//generic_ecs_service"
+  environment               = "${var.environment}"
+  project                   = "${var.project}"
+  data_dir                  = "/var/www/html/ppv-web"
+  database_hostname         = "not_required"
+  app                       = "peculiar-ppv"
+  application_memory        = 128
+  http_rule_priority        = 4
+  domain                    = "dev.mettalloids.com"
+  url                       = "peculiarppv"
+}
