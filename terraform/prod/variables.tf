911662d5d35a6eaf6f9ffbb54e71085903deef8f
Ayotunde Salako <salakoayotunde@mettalloids.com>
2018-03-27 20:46:35 +0100

    fix outofmemoryerror, input RDS variables, increase deployment time, add new vpc resources

--- terraform/prod/variables.tf
+++ terraform/prod/variables.tf
@@ -2,6 +2,28 @@ variable "environment" { default = "prod" }
 variable "project" { default = "peculiar-ppv" }
 variable "project_billing_id" { default = "PECULIAR" }
 variable "aws_region" { default = "eu-west-1" }
+variable "cert_domain" { default = "*.mettalloids.com"}
+variable "route_53_zone" { default = "prod.mettalloids.com"}
+variable "cluster" { default = "mettalloids"}
+
+
+variable "http_listener_arn" {
+  type = "map"
+
+  default = {
+    dev = "arn:aws:elasticloadbalancing:eu-west-1:780172389235:listener/app/mettalloids-dev/2c2e026ae870668e/49e73f5a8e9fe775"
+    prod = "arn:aws:elasticloadbalancing:eu-west-1:780172389235:listener/app/mettalloids-prod/df2a714b0665a12c/0f72b1c0fa15cb1a"
+  }
+}
+
+variable "https_listener_arn" {
+  type = "map"
+
+  default = {
+    dev = "arn:aws:elasticloadbalancing:eu-west-1:780172389235:listener/app/mettalloids-dev/2c2e026ae870668e/06318349eabb9df7"
+    prod = "arn:aws:elasticloadbalancing:eu-west-1:780172389235:listener/app/mettalloids-prod/df2a714b0665a12c/58e01562bf328c86"
+  }
+}
 
 # RDS
 variable "db_engine_type" { default="mysql" }
