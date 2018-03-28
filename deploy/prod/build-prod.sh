60bd668b515919b2d660863d622d17b84eb88020
Mofesola Babalola <me@mofesola.com>
2018-03-28 11:54:48 +0100

    chore: remove envsubst in wp-config

--- deploy/prod/build-prod.sh
+++ deploy/prod/build-prod.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 envsubst < ~/.aws/credentials.tmpl > ~/.aws/credentials
-envsubst < wp-config.php > wp-config2.php && mv wp-config2.php wp-config.php
 envsubst < scripts/newrelic.ini > scripts/newrelic2.ini && mv scripts/newrelic2.ini scripts/newrelic.ini
 eval $(aws ecr get-login --no-include-email --region ${AWS_REGION})
 
